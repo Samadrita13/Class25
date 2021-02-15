d6835abd5392c79f23d61a63a3de7a0f11d9e666
Samadrita13 <77491500+Samadrita13@users.noreply.github.com>
2021-02-15 17:56:21 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,14 @@
+class Bird extends base{
+    constructor(x, y) {
+
+      this.image = loadImage("sprites/bird.png")
+    };
+    display(){
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      super.display()
+
+    };
+  };
+  
